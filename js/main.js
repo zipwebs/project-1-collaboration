cfac32907da6ed54f18c9f715b3d5d3c9411e565
Basma2023 <basma.sayyad@gmail.com>
2023-11-08 18:59:07 -0500

    Function now in full effect, which will target the element that has the ID update-info

--- js/main.js
+++ js/main.js
@@ -2,4 +2,13 @@ let isUpdated = false; // flag to track the content state
 
 function toggleContent() {
     // This fuction will check if the desired element is empty, if it is then it will update it's inner HTML with some content
+    const paragraph = document.getElementById('update-info');
+
+    if (!isUpdated) {
+        paragraph.innerHTML = 'This HTML is updated by the function called <span class=\"blue-text\">toggleContent()<span>. If you click it again it will remove this html';
+    } else {
+        paragraph.innerHTML = '';
+    }
+
+    isUpdated = !isUpdated; // toggle the flag
 }
