6d47ef5f60e260dd6c9ba0bc3040131e77f2874c
Basma2023 <basma.sayyad@gmail.com>
2023-11-08 18:58:04 -0500

    Createdd the function called toggleContent

--- js/main.js
+++ js/main.js
@@ -1,1 +1,5 @@
 let isUpdated = false; // flag to track the content state
+
+function toggleContent() {
+    // This fuction will check if the desired element is empty, if it is then it will update it's inner HTML with some content
+}
